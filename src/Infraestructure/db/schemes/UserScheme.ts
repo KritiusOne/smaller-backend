a92aa91f6697a03e912794ef3adc1919fc5b7157
Julio Rodriguez Ureche <julio10489@gmail.com>
2025-12-03 17:00:17 -0500

    feat: add utils -> collection list helper and user scheme example

---
+++ src/Infraestructure/db/schemes/UserScheme.ts
@@ -0,0 +1,12 @@
+import {IUser } from "@src/Domain/entities/User"
+import { Schema, model } from 'mongoose';
+import { CollectionList } from '@src/helpers/collectionList';
+
+const UserScheme = new Schema<IUser>({
+  id: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+});
+
+const UserModel = model<IUser>(CollectionList.USERS, UserScheme);
+export { UserModel };
