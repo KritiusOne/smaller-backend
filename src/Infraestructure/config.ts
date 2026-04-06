e2564ce213d59e54655354c453c5b8508c55803d
KritiusOne <julio10489@gmail.com>
2026-04-05 21:01:40 -0500

    feat: remake config object

--- src/Infraestructure/config.ts
+++ src/Infraestructure/config.ts
@@ -2,18 +2,29 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-
+const {
+  queryString,
+  databaseNameDev,
+  PORT,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_CLIENT_EMAIL,
+  FIREBASE_PRIVATE_KEY,
+  FIREBASE_ADMIN_NAME,
+  FIREBASE_ADMIN_CONFIG_RAW
+} = process.env;
 export const config = {
-  queryString: process.env.queryString || '',
-  databaseNameDev: process.env.databaseNameDev || '',
-  port: parseInt(process.env.PORT || '3000', 10),
+  queryString: queryString || '',
+  databaseNameDev: databaseNameDev || '',
+  port: parseInt(PORT || '3000', 10),
   firebase: {
-    projectId: process.env.FIREBASE_PROJECT_ID || '',
-    privateKey: process.env.FIREBASE_PRIVATE_KEY
-      ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    projectId: FIREBASE_PROJECT_ID || '',
+    privateKey: FIREBASE_PRIVATE_KEY
+      ? FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
       : '',
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL || '', 
-    firebaseAdminConfigName: process.env.FIREBASE_ADMIN_NAME || 'smaller-backend',
-    firebaseAdminConfigRaw: process.env.FIREBASE_ADMIN_CONFIG_RAW ? JSON.parse(process.env.FIREBASE_ADMIN_CONFIG_RAW) : null
+    clientEmail: FIREBASE_CLIENT_EMAIL || '', 
+    firebaseAdminConfigName: FIREBASE_ADMIN_NAME || 'smaller-backend',
+    firebaseAdminConfigRaw: FIREBASE_ADMIN_CONFIG_RAW ? JSON.parse(FIREBASE_ADMIN_CONFIG_RAW) : null
   }
 };
+
+console.log(config)
